0075e3906c6e08cc0d3513f2b2a3fc2d77df4b8a
Qinmei <sun.houtao@outlook.com>
2021-12-04 16:09:39 +0800

    feature: test build scripts

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,78 +1,76 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
 
-module.exports = opt => {
-  return {
-    mode: 'production',
-    entry: path.resolve(opt.path, './src/index'),
-    output: {
-      filename: 'index.js',
-      clean: true,
-      libraryTarget: "module",
-      path: path.resolve(__dirname, "./lib"),
-    },
-    module: {
-      rules: [
-        {
-          test: /.tsx?$/,
-          use: ['babel-loader', 'ts-loader'],
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.less$/,
-          exclude: /node_modules/,
-          use: [
-            'style-loader',
-            {
-              loader: 'css-loader',
-              options: {
-                modules: {
-                  mode: 'local',
-                  localIdentName: '[local]-[hash:base64:6]',
-                },
-              },
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: [autoprefixer],
-              },
-            },
-            'less-loader',
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: [autoprefixer],
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(png|jpg|svg)$/i,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 1024,
-              },
-            },
-          ],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '...'],
-    },
-    externals: ['react'],
-    experiments: {
-        outputModule: true,
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, './src/index'),
+  output: {
+    filename: 'index.js',
+    clean: true,
+    libraryTarget: 'module',
+    path: path.resolve(__dirname, './lib'),
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/,
       },
-  };
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[local]-[hash:base64:6]',
+              },
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [autoprefixer],
+            },
+          },
+          'less-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [autoprefixer],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '...'],
+  },
+  externals: ['react'],
+  experiments: {
+    outputModule: true,
+  },
 };
--- scripts/build.js
+++ scripts/build.js
@@ -1,12 +1,10 @@
 const webpack = require('webpack');
-const fs = require('fs');
-const path = require('path');
 
 const webpackConfig = require('../config/webpack.config');
 
-function build(configs) {
-  // 遍历执行配置项
-  webpack(webpackConfig(config), (err, stats) => {
+function build() {
+  console.log(__dirname);
+  webpack(webpackConfig, (err, stats) => {
     if (err) {
       console.error(err);
       return;
@@ -21,11 +19,10 @@ function build(configs) {
     if (stats.hasErrors()) {
       return;
     }
-    console.log(`${config.name} build successed!`);
+    console.log(`build successed!`);
   });
 }
 
 console.log('\n===> running build');
 
-// 执行所有配置
-build(Object.values(mapPackages()));
+build();
