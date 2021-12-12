991f4eaa913cf78c909a519829013a4e21ee545a
Qinmei <sun.houtao@outlook.com>
2021-12-12 13:25:33 +0800

    feature: add config script

---
+++ scripts/config.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const copyTemplateFile = (path)=>{
+    const files = fs.readdirSync(path.resolve(__dirname,path));
+    if(!files)
+}
+fs.readdir
